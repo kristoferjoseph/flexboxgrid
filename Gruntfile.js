8e40bf3c0ed62a0a26926a4df4114362dee9be31
kristoferjoseph <kristoferjoseph@gmail.com>
2013-12-22 00:24:32 -0800

    First pass at flexbox grid

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        autoprefixer: {
+            compile: {
+                src: 'src/grid.css',
+                dest: 'css/flexgrid.css'
+            }
+        },
+        cssmin: {
+            minify: {
+                expand: true,
+                cwd: 'css',
+                src: ['*.css', '!*.min.css'],
+                dest: 'css',
+                ext: '.min.css'
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+
+    // Default task.
+    grunt.registerTask('default', ['autoprefixer', 'cssmin']);
+
+};
