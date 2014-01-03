b38e9dc3f6e0530dd63c1be484a3781d96e3880f
kristoferjoseph <kristoferjoseph@gmail.com>
2014-01-02 19:33:34 -0800

    Adding auto close to sections

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,15 @@
       q = d.querySelector,
       menu = d.querySelector('.js-menu'),
       menuButton = d.querySelector('.js-menu-button'),
+      menuList = d.querySelector('.js-menu__list'),
       page = d.querySelector('.page');
 
   menu.classList.add('slide-menu');
   menuButton.addEventListener('click', function() {
+    page.classList.toggle('open');
+  });
+
+  menuList.addEventListener('click', function() {
     page.classList.toggle('open');
   });
 
