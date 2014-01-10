3451f90ae4503692d795a4d42678712d0ebf5f52
kristoferjoseph <kristoferjoseph@gmail.com>
2014-01-10 01:06:13 -0800

    Optimized external js and icon loading

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,33 @@
-(function(window){
-  var d = window.document,
-      q = d.querySelector,
-      menu = d.querySelector('.js-menu'),
-      menuButton = d.querySelector('.js-menu-button'),
-      menuList = d.querySelector('.js-menu__list'),
-      page = d.querySelector('.page');
+(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function()
+    {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-  menu.classList.add('slide-menu');
-  menuButton.addEventListener('click', function() {
-    page.classList.toggle('open');
-  });
-
-  menuList.addEventListener('click', function() {
-    page.classList.toggle('open');
-  });
-
-}(this));
-
+ga('create', 'UA-46683011-1', 'flexboxgrid.com');
+ga('send', 'pageview');
+(function() {
+    var config = {
+      kitId: 'nod8myc'
+    };
+    var d = false;
+    var tk = document.createElement('script');
+    tk.src = '//use.typekit.net/' + config.kitId + '.js';
+    tk.type = 'text/javascript';
+    tk.async = 'true';
+    tk.onload = tk.onreadystatechange = function() {
+      var rs = this.readyState;
+      if (d || rs && rs != 'complete' && rs != 'loaded') return;
+      d = true;
+      try { Typekit.load(config); } catch (e) {}
+    };
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(tk, s);
+  })();
