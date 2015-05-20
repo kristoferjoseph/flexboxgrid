83eae3a9a83668caa0dbdbfffecff1d1f26b63ca
Kristofer Joseph <kristoferjoseph@gmail.com>
2015-05-20 14:04:13 -0700

    Initial pass at setting up multiple sources

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'),
-  instant = require('instant'),
-  colors = require('colors'),
-  app = express();
-
-app.use(instant(__dirname));
-app.listen(3000,
-  console.log('Navigate to ' + 'http://localhost:3000'.green + ' to view your page.')
-);
