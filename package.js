8510f2df8d79de6ff672c1df3571b54405d6441d
Jimmie <jimmiebtlr@gmail.com>
2016-03-24 22:40:07 -0500

    Package for meteor.

---
+++ package.js
@@ -0,0 +1,13 @@
+Package.describe({
+  name: 'kristoferjoseph:flexboxgrid',
+  version: '0.6.3',
+  summary: 'Grid based off of CSS3 flexbox specification',
+  git: 'https://github.com/kristoferjoseph/flexboxgrid',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.3-rc.8');
+  api.use('nathantreid:css-modules');
+  api.addFiles('css/flexboxgrid.css');
+});
