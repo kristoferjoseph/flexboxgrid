f17c256feaef774d7a4b9136ba87d121843e6287
Kristofer Joseph <kristoferjoseph@gmail.com>
2016-08-11 22:15:45 -0700

    First pass

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = function flexboxgrid(options) {
+  options = options || {}
+  var columns = options.columns || 12
+  var padding = options.padding || '2rem'
+  var gutters = options.gutters || padding * 0.5
+  var breakPoints = options.breakPoints || {xs:30,sm:48,md:64,lg:75}
+
+
+}
--- js/index.js
+++
@@ -1,1 +0,0 @@
-!function(a,b,c,d,e,f,g){a.GoogleAnalyticsObject=e,a[e]=a[e]||function(){(a[e].q=a[e].q||[]).push(arguments)},a[e].l=1*new Date,f=b.createElement(c),g=b.getElementsByTagName(c)[0],f.async=1,f.src=d,g.parentNode.insertBefore(f,g)}(window,document,"script","//www.google-analytics.com/analytics.js","ga"),ga("create","UA-46683011-1","flexboxgrid.com"),ga("send","pageview");
