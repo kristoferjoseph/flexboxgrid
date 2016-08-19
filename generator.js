e0ae17811d376d4533cf0af9714e2ce37237ea83
Kristofer Joseph <kristoferjoseph@gmail.com>
2016-08-19 08:03:53 -0700

    Adds command line options parsing.

--- generator.js
+++ generator.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
+var opts        = require('minimist')(process.argv.slice())
 var flexboxgrid = require('./index')
-process.stdout.write(flexboxgrid().output)
+process.stdout.write(flexboxgrid(opts).getGrid())
